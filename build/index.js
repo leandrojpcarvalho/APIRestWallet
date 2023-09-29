b69adcf651d3d17618b90cb8fea9bae3e31b466f
Leandro <ljpcarvalho@gmail.com>
2023-09-29 11:16:39 -0300

    delete build

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Server_1 = require("./server/Server");
-Server_1.server.listen(process.env.PORT, () => {
-    console.log(`app rodando na porta ${process.env.PORT}!`);
-});
