da25c8ce0561fa07dade97b4c5ae19fabacfa6ff
Leandro <ljpcarvalho@gmail.com>
2023-09-29 15:15:20 -0300

    de volta a build

---
+++ build/server/routes/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const controllers_1 = require("../controllers");
+const router = (0, express_1.Router)();
+router.get('/', (_, res) => {
+    return res.send('API REST Wallet');
+});
+router.post('/users', controllers_1.UsersControllers.createValidation, controllers_1.UsersControllers.create);
+router.put('/users/:id', controllers_1.UsersControllers.updateByIdValidation, controllers_1.UsersControllers.updateById);
+router.get('/users', controllers_1.UsersControllers.getAllValidation, controllers_1.UsersControllers.getAll);
+router.get('/users/:id', controllers_1.UsersControllers.getByIdValidation, controllers_1.UsersControllers.getById);
+router.delete('/users/:id', controllers_1.UsersControllers.deleteByIdValidation, controllers_1.UsersControllers.deleteById);
+exports.default = router;
